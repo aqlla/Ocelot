a2bd30adc4dc1fc024efa4dd6dea237450a6401f
Aquilla Sherrock <acsherrock@gmail.com>
2015-03-12 15:41:35 -0500

    fix cron

---
+++ cronrun.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+cd /Users/acsherrock/Documents/Programming/Python/amazon
+source ve/bin/activate
+python run.py
+
---
+++ run.py
@@ -0,0 +1,3 @@
+#!/Users/acsherrock/Documents/Programming/Python/amazon/ve/bin/python
+import ocelot
+ocelot.test()
